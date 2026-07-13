318f2d333df48f09a7c79bbf7068033aac90fded
saadWaseem3 <saad3102004@gmail.com>
2026-07-13 23:19:57 +0500

    Started working on creating the medicine class

---
+++ Medicines.cs
@@ -0,0 +1,11 @@
+namespace PharmacyApp
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Medicine";
+        public string Category { get; set; } = "Category";
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
